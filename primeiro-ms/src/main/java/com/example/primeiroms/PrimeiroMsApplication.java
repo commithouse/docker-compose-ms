b075d19dd3ddd1e160c8add02cd17f1135775eb8
Daniel Lemeszenski <tercmt-daniell@ftd.com.br>
2022-10-25 15:26:26 -0300

    inicial

---
+++ primeiro-ms/src/main/java/com/example/primeiroms/PrimeiroMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.primeiroms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrimeiroMsApplication.class, args);
+    }
+
+}
---
+++ primeiro-ms/src/test/java/com/example/primeiroms/PrimeiroMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.primeiroms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
